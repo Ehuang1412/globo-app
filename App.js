dffcd564822dd0984331ba67627a75966f3b1c58
Emily Huang <eh1691@nyu.edu>
2021-10-30 19:07:18 -0400

    Setup routing and header

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer } from'@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Homepage from './Home';
+import { Platform } from 'react-native';
+import { useFonts } from 'expo-font';
+// import {  Inter_900Black } from '@expo-google-fonts/inter';
+// import AppLoading from 'expo';
+import AppLoading from 'expo-app-loading';
+import Header from './Header';
+
+//define route
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    // 'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if(!fontsLoaded) {
+    return <AppLoading/>;
+  }
+  else{
+    return (
+      <NavigationContainer
+        style={{paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight:0}}
+      >
+      {/*header is part of the screen*/}
+        <Stack.Navigator 
+          initialRouteName="Globomantics"
+          headerMode="screen" 
+        >
+        {/*//route*/}
+        
+          <Stack.Screen  
+            name="Globomantics"
+            components={Homepage}
+            options={{
+              header: ()=> <Header headerDisplay="Globomantics" />
+            }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+// expo install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
