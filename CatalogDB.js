dde78dff57692bafd71b59c41e3b969edc7c56d1
Emily Huang <eh1691@nyu.edu>
2021-11-03 16:04:31 -0400

    Setup Catalog and its details

---
+++ CatalogDB.js
@@ -0,0 +1,109 @@
+const globoCatalog = [
+  {
+    model: 'Scientific Robotics',
+    image: require('./assets/NYUgradshotHandshake3.PNG'),
+    modelNumber: 'SCR-1',
+    description: `
+yì xiǎng dào nǐ wǒ jiù 
+      一 想    到  你 我 就  
+      Wu
+      kōng hèn bié mèng jiǔ 
+      空   恨  别  梦   久  
+      Wu
+      shāo qù zhǐ huī mái yān liǔ 
+      烧   去 纸  灰  埋  烟  柳  
+      yú xiān huó de zhī yā 
+      于 鲜   活  的 枝  丫 
+      diāo líng xià de wú xiá 
+      凋   零   下  的 无 暇  
+      shì shōu huò mí dǐ de dài jià 
+      是  收   获  谜 底 的 代  价 
+    `
+  },
+  {
+    model: 'Diagnostic Robotics',
+    image: require('./assets/NYUgradshotHandshake3.PNG'),
+    modelNumber: 'DIR-2',
+    description: `
+      yì xiǎng dào nǐ wǒ jiù 
+      一 想    到  你 我 就  
+      Wu
+      kōng hèn bié mèng jiǔ 
+      空   恨  别  梦   久  
+      Wu
+      shāo qù zhǐ huī mái yān liǔ 
+      烧   去 纸  灰  埋  烟  柳  
+      yú xiān huó de zhī yā 
+      于 鲜   活  的 枝  丫 
+      diāo líng xià de wú xiá 
+      凋   零   下  的 无 暇  
+      shì shōu huò mí dǐ de dài jià 
+      是  收   获  谜 底 的 代  价 
+    `
+  },
+  {
+    model: 'Surgical Robotics',
+    image: require('./assets/NYUgradshotHandshake3.PNG'),
+    modelNumber: 'SUR-3',
+    description: `
+      yì xiǎng dào nǐ wǒ jiù 
+      一 想    到  你 我 就  
+      Wu
+      kōng hèn bié mèng jiǔ 
+      空   恨  别  梦   久  
+      Wu
+      shāo qù zhǐ huī mái yān liǔ 
+      烧   去 纸  灰  埋  烟  柳  
+      yú xiān huó de zhī yā 
+      于 鲜   活  的 枝  丫 
+      diāo líng xià de wú xiá 
+      凋   零   下  的 无 暇  
+      shì shōu huò mí dǐ de dài jià 
+      是  收   获  谜 底 的 代  价 
+    `
+  },
+  {
+    model: 'Agricultural Robotics',
+    image: require('./assets/NYUgradshotHandshake3.PNG'),
+    modelNumber: 'AGR-4',
+    description: `
+      yì xiǎng dào nǐ wǒ jiù 
+      一 想    到  你 我 就  
+      Wu
+      kōng hèn bié mèng jiǔ 
+      空   恨  别  梦   久  
+      Wu
+      shāo qù zhǐ huī mái yān liǔ 
+      烧   去 纸  灰  埋  烟  柳  
+      yú xiān huó de zhī yā 
+      于 鲜   活  的 枝  丫 
+      diāo líng xià de wú xiá 
+      凋   零   下  的 无 暇  
+      shì shōu huò mí dǐ de dài jià 
+      是  收   获  谜 底 的 代  价 
+    `
+  },
+  {
+    model: 'Office Robotics',
+    image: require('./assets/NYUgradshotHandshake3.PNG'),
+    modelNumber: 'OFR-1',
+    description: `
+      yì xiǎng dào nǐ wǒ jiù 
+      一 想    到  你 我 就  
+      Wu
+      kōng hèn bié mèng jiǔ 
+      空   恨  别  梦   久  
+      Wu
+      shāo qù zhǐ huī mái yān liǔ 
+      烧   去 纸  灰  埋  烟  柳  
+      yú xiān huó de zhī yā 
+      于 鲜   活  的 枝  丫 
+      diāo líng xià de wú xiá 
+      凋   零   下  的 无 暇  
+      shì shōu huò mí dǐ de dài jià 
+      是  收   获  谜 底 的 代  价 
+    `
+  },
+];
+export default globoCatalog;
+  
